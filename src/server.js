fcba98e27629f69ca5c0a37ecf5ac886caca2a79
nkellem <noahkellem@gmail.com>
2018-02-16 14:06:04 -0500

    Basic app finished

--- src/server.js
+++ src/server.js
@@ -16,14 +16,45 @@ const app = http.createServer(onRequest).listen(PORT);
 
 console.log(`Listening on localhost:${PORT}`);
 
+const determineNewSquare = () => {
+  let x = Math.floor(Math.random() * 1000);
+  let y = Math.floor(Math.random() * 700);
+  const width = 100;
+  const height = 100;
+
+  if (x + width >= 1000) {
+    x = 1000 - width;
+  }
+
+  if (y + height >= 700) {
+    y = 700 - height;
+  }
+
+  return {
+    x, y, width, height,
+  };
+};
+
 // pass in the http server into socketio and grab the websocket sever as io
 const io = socketio(app);
+// keep track of the current square
+let currentSquare = determineNewSquare();
 
 const onJoined = (sock) => {
   const socket = sock;
 
   socket.on('join', () => {
+    socket.emit('sendNewSquare', currentSquare);
     socket.join('room1');
+  });
+};
+
+const onSquareClick = (sock) => {
+  const socket = sock;
+
+  socket.on('squareClicked', () => {
+    currentSquare = determineNewSquare();
+    io.sockets.in('room1').emit('sendNewSquare', currentSquare);
   });
 };
 
@@ -39,6 +70,7 @@ io.sockets.on('connection', (socket) => {
   console.log('started');
 
   onJoined(socket);
+  onSquareClick(socket);
   onDisconnect(socket);
 });
 
